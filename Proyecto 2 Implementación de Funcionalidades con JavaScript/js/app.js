e6778bcf7b740517f8635df300cc22c28ae1a979
Debora MR <dmedina@iesrafaelalberti.es>
2023-11-12 16:51:29 +0100

    Commit

--- Proyecto 2 Implementación de Funcionalidades con JavaScript/js/app.js
+++ Proyecto 2 Implementación de Funcionalidades con JavaScript/js/app.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded',() => {
                         <td class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-black tracking-wider">${contacto.telefono}</td>
                         <td class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-black tracking-wider">${contacto.empresa}</td>
                         <td class="px-6 py-3 border-b border-gray-200  text-left text-xs leading-4 font-medium text-black tracking-wider">
-                            <a href="editar-cliente.html" id="modificar-cliente" type="button" class="mb-1 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modificar</a>
+                            <a href="#" id="modificar-${cursor.key}" type="button" class="mb-1 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modificar</a>
                             <br>
                             <a href="#" id="eliminar-${cursor.key}" type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"> Eliminar </a>
                         </td>
@@ -51,6 +51,20 @@ document.addEventListener('DOMContentLoaded',() => {
                     myId = parseInt(myId)
                     
                     eliminarContacto(myId)
+                })
+
+                // modificar la fila seleccionada
+                let modificarBtn = document.querySelector("#modificar-" + cursor.key);
+                // evento del boton
+                modificarBtn.addEventListener("click", (e)=>{
+                    let myId = e.target.id.split("-")[1];
+                    myId = parseInt(myId)
+                    
+                    //metodo de editarCliente.js
+                    modificarContacto(myId)
+                    // guardo ID en el localStorage
+                    localStorage.setItem('myId', myId)
+                    
                 })
 
                 cursor.continue();
